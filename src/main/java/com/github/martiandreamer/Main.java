feb4a8100a8a459d609e92f12ab5b18a93ef227b
Sang Nguyen <nguyenxuansang9494@gmail.com>
2025-11-04 13:42:49 +0700

    init commit

--- src/main/java/com/github/martiandreamer/Main.java
+++ src/main/java/com/github/martiandreamer/Main.java
@@ -1,9 +1,10 @@
 package com.github.martiandreamer;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+
 public class Main {
-    static void main() {
+    static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println(Arrays.toString(args));
     }
 }
